b1201921fe9be5c11bca9c212091f45c620d82ae
benjamin <benchude@gmail.com>
2022-07-19 17:55:23 +0100

    updated package.json

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section className="about">
       <div className="about-container">
-        <p>About Okey Ben Achike</p>
+        <p id='about'>About Okey Ben Achike</p>
         <div className='about-message'>
           <p>
             I'm a MERN (mongodb, express, react, nodejs) stack web developer interested in both frontend and backend work and collaboration. I acquired these skills to be useful to you seeking a presence in the web or those desiring to engage my skills. I give my best in my work and always strive  to improve on what I know.
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,22 +1,25 @@
 import React from 'react'
+import  { Link } from 'react-router-dom'
 import '../css/Landing.css'
 
 const Landing = () => {
   return (
     <section className="landing">
       <div className='landing-container'>
-        <img src="/assets/MERN-symbol.png" alt="" className='top-landing'/>
+        {/* <img src="/assets/MERN-symbol.png" alt="" className='top-landing'/> */}
         <div className='mid-landing'>
           <div className='intro'>
             <p>Hi, I'm Okey Ben Achike,</p>
             <p>and I'm a full stack Web Developer.</p>
           </div>
-          <img src="/assets/mongodb_symbol.png" alt="" />
+          <img src="/assets/mongodb_logo.jpg" alt="" />
         </div>
         <div className='bottom-landing'>
           <div className='bottom-landing-content'>
             <p>I'm ready to work</p>
-            <i className="fa fa-long-arrow-right fa_custom fa-2x"></i>
+            <a href="#about">
+              <i className="fa fa-long-arrow-right fa_custom fa-2x"></i>
+            </a>
           </div>
         </div>
       </div>
